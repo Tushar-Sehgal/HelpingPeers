f4dd19239c8495f41d3547ec723957c1f101898a
Tushar-Sehgal <tstusharsehgal123@gmail.com>
2024-05-30 09:26:28 +1000

    initial files

---
+++ 6.2D/BoxOfCoins.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BoxOfCoins
+{
+    public class BoxOfCoins
+    {
+
+        public static int Solve(int[] boxes)
+        {
+            // You should replace this plug by your code.
+            throw new NotImplementedException();
+        }
+        
+    }
+
+}
---
+++ 8.2HD/CoinRepresentation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CoinRepresentation
+{
+    public class CoinRepresentation
+    {
+        public static long Solve(long sum)
+        {
+            // You should replace this plug by your code.
+            throw new NotImplementedException();
+        }
+
+    }
+}
